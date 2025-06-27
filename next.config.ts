c011865a064c955ad94339ef361434a4ece7a8bd
sangwookpark <sangwookp9591@gmail.com>
2025-06-27 17:25:32 +0900

    feat : vanilla extract install

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,26 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
+
+const {
+    createVanillaExtractPlugin,
+    // eslint-disable-next-line @typescript-eslint/no-require-imports
+} = require('@vanilla-extract/next-plugin');
+const withVanillaExtract = createVanillaExtractPlugin();
 
 const nextConfig: NextConfig = {
-  /* config options here */
+    /* config options here */
+    // experimental: {
+    //     serverActions: {
+    //         bodySizeLimit: '10mb', // Set the body size limit for server actions
+    //     },
+    // },
+    // async rewrites() {
+    //     return [
+    //         {
+    //             source: '/upload/:slug',
+    //             destination: `${process.env.NEXT_PUBLIC_BASE_URL}/upload/:slug`, // Matched parameters can be used in the destination
+    //         },
+    //     ];
+    // },
 };
 
-export default nextConfig;
+module.exports = withVanillaExtract(nextConfig);
