e482864886343f9a12062c5f1702537b7d969a22
sangwookpark <sangwookp9591@gmail.com>
2025-07-02 07:54:54 +0900

    refactor : 미사용 삭제

--- src/app/api/tournaments/[id]/applications/[applicationId]/route.ts
+++ src/app/api/tournaments/[id]/applications/[applicationId]/route.ts
@@ -1,5 +1,5 @@
 import { NextRequest, NextResponse } from 'next/server';
-import { PrismaClient, ApplicationStatus } from '@prisma/client';
+import { PrismaClient } from '@prisma/client';
 
 const prisma = new PrismaClient();
 
