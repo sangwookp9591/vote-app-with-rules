5e6f18d6bef145fb65c12279d4bf0123943ddde2
sangwookpark <sangwookp9591@gmail.com>
2025-07-02 03:13:30 +0900

    feat: next-auth sessionprovider 추가

--- src/app/AppShell.tsx
+++ src/app/AppShell.tsx
@@ -5,20 +5,23 @@ import Sidebar from '../widgets/Sidebar/Sidebar';
 import Navbar from '../widgets/Navbar/Navbar';
 import Provider from '../context/ThemeProvider';
 import ThemeColorUpdater from '../shared/ui/ThemeColorUpdater';
+import AuthProvider from '@/context/AuthProvider';
 
 export default function AppShell({ children }: { children: React.ReactNode }) {
   const [sidebarOpen, setSidebarOpen] = useState(true);
 
   return (
     <>
-      <Sidebar open={sidebarOpen} />
-      <div style={{ marginLeft: sidebarOpen ? 240 : 0, transition: 'margin-left 0.2s' }}>
-        <Navbar onSidebarToggle={() => setSidebarOpen((v) => !v)} sidebarOpen={sidebarOpen} />
-        <Provider>
-          <ThemeColorUpdater />
-          {children}
-        </Provider>
-      </div>
+      <AuthProvider>
+        <Sidebar open={sidebarOpen} />
+        <div style={{ marginLeft: sidebarOpen ? 240 : 0, transition: 'margin-left 0.2s' }}>
+          <Navbar onSidebarToggle={() => setSidebarOpen((v) => !v)} sidebarOpen={sidebarOpen} />
+          <Provider>
+            <ThemeColorUpdater />
+            {children}
+          </Provider>
+        </div>
+      </AuthProvider>
     </>
   );
 }
