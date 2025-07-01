12db812a19108a17e89c53268f74250f717ab84c
sangwookpark <sangwookp9591@gmail.com>
2025-07-01 23:39:53 +0900

    fix(storybook): update import statement for Storybook to use '@storybook/react'

--- src/features/counter/ui/Counter.stories.tsx
+++ src/features/counter/ui/Counter.stories.tsx
@@ -1,4 +1,5 @@
-import type { Meta, StoryObj } from '@storybook/nextjs';
+// eslint-disable-next-line storybook/no-renderer-packages
+import type { Meta, StoryObj } from '@storybook/react';
 import { Counter } from './Counter';
 
 const meta: Meta<typeof Counter> = {
