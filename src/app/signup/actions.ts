9f128dbdc424592b510e177120a3ee23beb597f8
sangwookpark <sangwookp9591@gmail.com>
2025-07-02 11:24:33 +0900

    feat : 임시 관리자 아이디 추가

--- src/app/signup/actions.ts
+++ src/app/signup/actions.ts
@@ -64,7 +64,7 @@ export async function signupAction(
         password: hashedPassword,
         nickname,
         profileImageUrl,
-        role: email === 'admin@admin.com' ? 'ADMIN' : undefined,
+        role: email === 'admin2@admin.com' ? 'ADMIN' : undefined,
       },
     });
     return { success: '회원가입이 완료되었습니다!' };
