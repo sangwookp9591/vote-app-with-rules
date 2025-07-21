4c64fc147f897676bac525654e7b24cf8ca68e46
sangwookpark <sangwookp9591@gmail.com>
2025-07-21 19:12:12 +0900

    feat(UserDetailWidget) : User detail widget파일 추가

---
+++ src/widgets/UserDetailWidget/UserDetailWidget.tsx
@@ -0,0 +1,12 @@
+'use client';
+
+import UserDetailCard from '@/features/user-detail/ui/UserDetailCard';
+
+export default function UserDetailWidget() {
+  return (
+    <div>
+      UserDetailWidget
+      <UserDetailCard />
+    </div>
+  );
+}
