d5cd8b3e499bd5d8b8ab717968e9b05d5640c169
sangwookpark <sangwookp9591@gmail.com>
2025-07-21 19:42:46 +0900

    refactor : user Detail type 수정

--- src/entities/user/detail/model/types.ts
+++ src/entities/user/detail/model/types.ts
@@ -4,12 +4,9 @@ import type { UserRole } from '../../model/types';
 // User의 상세 정보를 나타내는 인터페이스
 export interface UserDetail {
   id: string;
-  email: string;
   nickname: string;
-  name?: string;
   profileImageUrl?: string;
   role: UserRole;
   createdAt: string;
-  updatedAt: string;
   followerCount: number;
 }
