87fe39200fdf46c9accaf50539ffef5e6006e1c3
sangwookpark <sangwookp9591@gmail.com>
2025-07-22 00:26:36 +0900

    feat : user 조회 api추가

---
+++ src/features/user/api/route.ts
@@ -0,0 +1,9 @@
+import { User } from '@/entities/user';
+
+const API_BASE = '/api/user/';
+
+export async function fetchUser(id: string): Promise<User> {
+  const res = await fetch(`${API_BASE}/${id}`);
+  if (!res.ok) throw new Error('유저 조회 실패');
+  return res.json();
+}
