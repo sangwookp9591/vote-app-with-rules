34990223bbd21a1774ffee85db849c584d439277
sangwookpark <sangwookp9591@gmail.com>
2025-07-21 19:42:30 +0900

    feat:  user detail API endpoint 구현

---
+++ src/app/api/user/[id]/route.ts
@@ -0,0 +1,34 @@
+import { prisma } from '@/shared/prisma/client';
+import { NextRequest, NextResponse } from 'next/server';
+
+export async function GET(req: NextRequest, { params }: { params: Promise<{ id: string }> }) {
+  const { id } = await params;
+
+  if (!id)
+    return NextResponse.json({ error: 'Bad Request - id가 존재하지 않습니다.' }, { status: 400 });
+
+  const user = await prisma.user.findUnique({
+    where: {
+      id: id,
+    },
+    select: {
+      id: true,
+      nickname: true,
+      profileImageUrl: true,
+      _count: {
+        select: {
+          followers: true,
+        },
+      },
+    },
+  });
+  if (!user) return NextResponse.json({ error: 'not found ' }, { status: 404 });
+
+  const { _count, ...userWithoutCount } = user;
+  const userWithFollowerCount = {
+    ...userWithoutCount,
+    followerCount: _count.followers,
+  };
+
+  return NextResponse.json(userWithFollowerCount);
+}
