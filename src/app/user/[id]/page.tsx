cde1daf3c8b81c7415b59a664ab05872a4ab5a29
sangwookpark <sangwookp9591@gmail.com>
2025-07-21 19:39:28 +0900

    feat : UserDetail page 추가

---
+++ src/app/user/[id]/page.tsx
@@ -0,0 +1,9 @@
+import UserDetailWidget from '@/widgets/UserDetailWidget/UserDetailWidget';
+
+export default function UserDetail() {
+  return (
+    <div>
+      <UserDetailWidget />
+    </div>
+  );
+}
