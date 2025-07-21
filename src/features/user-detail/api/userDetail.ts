64c053917e6317ef41a1946dc64289c70139de71
sangwookpark <sangwookp9591@gmail.com>
2025-07-21 19:39:12 +0900

    feat : userDetail api 추가

---
+++ src/features/user-detail/api/userDetail.ts
@@ -0,0 +1,9 @@
+import { UserDetail } from '@/entities/user/detail';
+
+const API_BASE = '/api/user/';
+
+export async function fetchUserDetail(id: string): Promise<UserDetail[]> {
+  const res = await fetch(`${API_BASE}/${id}`);
+  if (!res.ok) throw new Error('유저 상세 조회 실패');
+  return res.json();
+}
