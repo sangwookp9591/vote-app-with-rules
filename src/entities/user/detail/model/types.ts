90270cc0e38feb24dfa6ea282405354cb8d3602c
sangwookpark <sangwookp9591@gmail.com>
2025-07-22 18:04:07 +0900

    feat : UserDetail에 streamer type 추가

--- src/entities/user/detail/model/types.ts
+++ src/entities/user/detail/model/types.ts
@@ -1,4 +1,5 @@
 // User 타입을 import해서 사용
+import { Streamer } from '@/entities/streamer/model/types';
 import type { UserRole } from '../../model/types';
 
 // User의 상세 정보를 나타내는 인터페이스
@@ -9,4 +10,5 @@ export interface UserDetail {
   role: UserRole;
   createdAt: string;
   followerCount: number;
+  streamer: Streamer;
 }
