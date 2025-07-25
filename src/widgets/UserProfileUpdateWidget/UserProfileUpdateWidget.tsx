5821c8f0e6a62224be15334f2c7bef5e93f06802
sangwookpark <sangwookp9591@gmail.com>
2025-07-25 14:27:28 +0900

    feat: integrate user fetching in UserProfileUpdateWidget to display user data in UserProfileEditForm

--- src/widgets/UserProfileUpdateWidget/UserProfileUpdateWidget.tsx
+++ src/widgets/UserProfileUpdateWidget/UserProfileUpdateWidget.tsx
@@ -1,9 +1,16 @@
+'use client';
+
 import UserProfileEditForm from '@/features/user-profile/ui/UserProfileEditForm';
+import { fetchUser } from '@/features/user/api/user';
+import { useQuery } from '@tanstack/react-query';
+import { useSession } from 'next-auth/react';
 
 export default function UserProfileUpdateWidget() {
-  return (
-    <>
-      <UserProfileEditForm />
-    </>
-  );
+  const { data: session } = useSession();
+  const { data: user } = useQuery({
+    queryKey: ['fetchUser', session?.user?.id],
+    queryFn: () => fetchUser(session?.user?.id || ''),
+    enabled: !!session?.user?.id,
+  });
+  return <>{user && <UserProfileEditForm user={user} />}</>;
 }
