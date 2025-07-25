8f6c6707261df0219d962bd2b85839c9715e7d8e
sangwookpark <sangwookp9591@gmail.com>
2025-07-25 14:27:06 +0900

    feat: updateMyProfile 및 changePassword 함수에 userId 추가 및 API 경로 수정

--- src/features/user-profile/api/userProfile.ts
+++ src/features/user-profile/api/userProfile.ts
@@ -1,10 +1,13 @@
+'use server';
+
 import { LocalStorageProvider } from '@/shared/storage/LocalStorageProvider';
 
-const API_URL = `/api/user/profile`;
+const API_URL = `/api/user/`;
+export type UpdateProfileState = { error?: string; success?: string };
 
-export async function updateMyProfile(formData: FormData) {
+export async function updateMyProfile(prevState: UpdateProfileState, formData: FormData) {
   const nickname = formData.get('nickname') as string;
-
+  const userId = formData.get('userId') as string;
   // 프로필 이미지 파일 저장
   let profileImageUrl: string | undefined = undefined;
   const profileImage = formData.get('profileImage');
@@ -12,17 +15,17 @@ export async function updateMyProfile(formData: FormData) {
     const storage = new LocalStorageProvider();
     profileImageUrl = await storage.upload(profileImage as File, { folder: 'user' });
   }
-  const res = await fetch(API_URL, {
+  const res = await fetch(`${API_URL}${userId}/profile`, {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify({ nickname, profileImageUrl }),
+    body: JSON.stringify({ userId, nickname, profileImageUrl }),
   });
   if (!res.ok) throw new Error('이용자 정보 수정 실패');
   return res.json();
 }
 
-export async function changePassword(data: { password: string; newPassword: string }) {
-  const res = await fetch(`${API_URL}/changePassword`, {
+export async function changePassword(data: { id: string; password: string; newPassword: string }) {
+  const res = await fetch(`${API_URL}/${data?.id}/profile/changePassword`, {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify(data),
